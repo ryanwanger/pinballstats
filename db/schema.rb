cc9bcbab64268fd77879ccdee8c76912ef4ab1a5
Ryan Wanger <ryanwanger@gmail.com>
2015-01-31 21:01:44 -0700

    Start using nickname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201000646) do
+ActiveRecord::Schema.define(version: 20150201034116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150201000646) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
   end
 
   create_table "scores", force: true do |t|
