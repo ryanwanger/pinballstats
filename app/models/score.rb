ff8bf5fefabe6c82cceaea77c0740e73e1edb272
Ryan Wanger <ryanwanger@gmail.com>
2015-05-18 09:40:31 -0600

    Why was I looping through all of them?

--- app/models/score.rb
+++ app/models/score.rb
@@ -19,18 +19,16 @@ class Score < ActiveRecord::Base
   end
 
   def calculate_points
-    self.league_game.scores.each do |score|
-      if league.uses_matchups?
-        total_scores = score.league_game.scores.where("id <> ?", score.id).count
-        lower_scores = score.league_game.scores.where("score < ?", score.score).count
-        outscored_percentage = (lower_scores.to_f / total_scores)*100
-        
-        score.update_column(:outscored_percentage, outscored_percentage)
-        score.update_column(:team_win, team_win?)
-      elsif league.uses_groups?
-        score.update_column(:points, score.points_awarded)
-      end
-        
+    if league.uses_matchups?
+      total_scores = league_game.scores.where("id <> ?", id).count
+      lower_scores = league_game.scores.where("score < ?", score).count
+      outscored_percentage = (lower_scores.to_f / total_scores)*100
+      
+      self.outscored_percentage = outscored_percentage
+      self.team_win = team_win?
+      self.save
+    elsif league.uses_groups?
+      update_column(:points, points_awarded)
     end
   end
 
