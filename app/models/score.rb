f81d0cba3faafc20380078c6f3ee7aff91107f98
Ryan Wanger <ryanwanger@gmail.com>
2015-05-18 09:32:05 -0600

    Do different scoring for different league types.

--- app/models/score.rb
+++ app/models/score.rb
@@ -14,14 +14,23 @@ class Score < ActiveRecord::Base
     return points
   end
 
+  def league
+    league_game.league_night.league
+  end
+
   def calculate_points
-    self.league_game.scores.where(group: self.group).each do |score|
-      total_scores = score.league_game.scores.where("id <> ?", score.id).count
-      lower_scores = score.league_game.scores.where("score < ?", score.score).count
-      win_percent = (lower_scores.to_f / total_scores)*100
-      score.update_column(:points, score.points_awarded)
-      score.update_column(:outscored_percentage, win_percent)
-      score.update_column(:team_win, team_win?)
+    self.league_game.scores.each do |score|
+      if league.uses_matchups?
+        total_scores = score.league_game.scores.where("id <> ?", score.id).count
+        lower_scores = score.league_game.scores.where("score < ?", score.score).count
+        outscored_percentage = (lower_scores.to_f / total_scores)*100
+        
+        score.update_column(:outscored_percentage, outscored_percentage)
+        score.update_column(:team_win, team_win?)
+      elsif league.uses_groups?
+        score.update_column(:points, score.points_awarded)
+      end
+        
     end
   end
 
