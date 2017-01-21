fa15475152f177c6c71dbe9da9c7ed9cbcafdd64
Ryan Wanger <ryanwanger@gmail.com>
2017-01-21 14:10:30 -0700

    Add nickname

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -76,6 +76,6 @@ class PlayersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def player_params
-      params.require(:player).permit(:name, :email)
+      params.require(:player).permit(:name, :email, :nickname)
     end
 end
